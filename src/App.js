f8204e379dd93f487e259076205f7e5171336410
Austinjames99 <james.aj.austin@gmail.com>
2023-07-19 23:20:39 -0400

    added post page/fixed bugs

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import { UserContextProvider } from './userContext';
+import PostPage from './pages/PostPage';
 import CreatePost from './pages/CreatePost';
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
         <Route path={'/login'} element={<LoginPage />} />
         <Route path={'/register'} element={<RegisterPage />} />
         <Route path={'/create'} element={<CreatePost />} />
+        <Route path={'/post/:id'} element={<PostPage />} />
       </Route>
     </Routes>
     </UserContextProvider>
