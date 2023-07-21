89c54864d3d39a1447a5a757dd696bbdef5dd492
Austinjames99 <james.aj.austin@gmail.com>
2023-07-20 21:38:48 -0400

    finished server setup

--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import {formatISO9075} from 'date-fns'
+import Image from "../Image"
+
 
 export default function PostPage(){
     const [postInfo, setPostInfo] = useState(null)
@@ -20,7 +22,7 @@ export default function PostPage(){
         <time>{formatISO9075(new Date(postInfo.createdAt),)}</time>
         <div className="author">{postInfo.author.username}</div>
       <div className="image">
-          <img src={`http://localhost:4000/${postInfo.cover}`} alt=''/>
+          <Image src={postInfo.cover} alt=''/>
       </div>
       
       <div dangerouslySetInnerHTML={{__html:postInfo.content}} />
--- src/post.js
+++ src/post.js
@@ -1,12 +1,14 @@
 import {formatISO9075} from 'date-fns' //used to make date look good/same as database
 import { Link } from 'react-router-dom'
+import Image from './Image'
+
 
 export default function Post({_id,title,summary,cover,content,createdAt,author}) {
     return(
         <div className='post'>
         <div className='image'>
           <Link to={`/post/${_id}`}>
-          <img src={'http://localhost:4000/'+cover} alt=''/>
+          <Image src={cover} alt=''/>
           </Link>
         </div>
         <div className='text'>
